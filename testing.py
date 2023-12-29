c1e2057af988154dc0fa37918cb7bac41c0227a6
flatypus [mac] <yhc3141@gmail.com>
2023-12-28 21:59:52 -0800

    feat: update with filtering useless notes

--- testing.py
+++ testing.py
@@ -5,6 +5,7 @@ import os
 import random
 
 target_path = "c-major-scale.mp3"
+out_path = "approximation-" + target_path
 sfx_path = "sfx/"
 
 song = AudioSegment.from_file(file=target_path)
@@ -44,6 +45,13 @@ def construct(song, num, volume, start):
     return sfx
 
 
+def full_construct(variations):
+    new_song = song - 10000
+    for variation in variations:
+        new_song = construct(new_song, *variation)
+    return new_song
+
+
 def variation(current_song):
     num = random.randrange(len(names))
     volume = random.randrange(-10, 1)
@@ -52,17 +60,62 @@ def variation(current_song):
     return [mse(song, new_song), [num, volume, start]]
 
 
-# essentially quiet
-base_song = song - 1000
-best_mse = 2**100
+def inverse(sfx):
+    frame_rate = sfx.frame_rate
+    sample_width = sfx.sample_width
+    channels = sfx.channels
 
-for i in range(10000):
+    array = np.array(sfx.get_array_of_samples())
+    array = np.negative(array)
+    raw_data = array.tobytes()
+
+    return AudioSegment(
+        data=raw_data,
+        sample_width=sample_width,
+        frame_rate=frame_rate,
+        channels=channels,
+    )
+
+
+def filter(best_mce, best_song, inverses):
+    if len(inverses) == 0:
+        return []
+
+    bad = []
+    for index, inverse in enumerate(inverses):
+        test = best_song.overlay(inverse)
+        diff = mse(song, test)
+        if diff < best_mce:
+            bad.append(index)
+
+    return bad
+
+
+# essentially quiet
+quiet = song - 10000
+base_song = song - 10000
+best_mse = 2**100
+best_v = []
+inverses = []
+
+for i in range(1000000):
+    if i % 10000 == 0 and i != 0:
+        print(f"Run: {i}: best: {best}")
+        bad = filter(best_mse, base_song, inverses)
+        best_v = [v for i, v in enumerate(best_v) if i not in bad]
+        inverses = [v for i, v in enumerate(inverses) if i not in bad]
+        base_song = full_construct(best_v)
+        best_mse = mse(song, base_song)
+        base_song.export(out_path)
+
     variations = [variation(base_song) for _ in range(10)]
     best = sorted(variations, key=lambda a: a[0])[0]
     if best[0] > best_mse:
         continue
     best_mse = best[0]
-    print(f"Run: {i}: best: {best}")
-    base_song = construct(base_song, *best[1])
+    best_v.append(best[1])
+    just_sound = construct(quiet, *best[1])
+    inverses.append(inverse(just_sound))
+    base_song = base_song.overlay(just_sound)
 
-base_song.export("approximation-" + target_path)
+base_song.export(out_path)
